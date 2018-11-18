f732e670ef9038e8f682921fcc244c11b20a4578
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-11-18 03:35:23 +0000

    all countries or no country

--- world.js
+++ world.js
@@ -1,23 +1,48 @@
 window.onload = function () {
     "use strict";
-    let lkp = document.getElementById("lookup");
-    lkp.addEventListener("click", exec);
-};
-
-function exec () {
+    let lkp = document.getElementById("lookup"); 
+    let check = document.getElementById("check");
+    
+    lkp.addEventListener("click", function exec () {
     let cntry = document.getElementById("country").value.trim();
     let httpRequest = new XMLHttpRequest();
-    let url = "https://info2180-lab7-lindsaypink.c9users.io/world.php/world.php?country=" + cntry;
-    httpRequest.onreadystatechange = function() {
+    
+    if (check.checked) {
+        let url = "https://info2180-lab7-lindsaypink.c9users.io/world.php/world.php?all=true";
+        httpRequest.onreadystatechange = function() {
         if (httpRequest.readyState === 4) {
             if (httpRequest.status === 200) {
                 let response = (httpRequest.responseText);
                 document.getElementById("result").innerHTML = response;
             } else {
-                alert("Problem with request.\nEnsure that search bar is not empty then check for spelling errors");
+                alert("We encountered an error. Try again later");
             }
         }
     };
     httpRequest.open("GET", url);
     httpRequest.send();
-} 
+    }
+    
+    else if ((!cntry) && (check.checked === false)) {
+        document.getElementById("result").innerHTML = "";
+        alert("Problem with request.\nEnsure that search bar is not empty then check for spelling errors");
+    }
+    
+    else {
+        let url = "https://info2180-lab7-lindsaypink.c9users.io/world.php/world.php?country=" + cntry;
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === 4) {
+                if (httpRequest.status === 200) {
+                    let response = (httpRequest.responseText);
+                    document.getElementById("result").innerHTML = response;
+                } else {
+                    alert("We encountered an error. Try again later");
+                }
+            }
+        };
+        httpRequest.open("GET", url);
+        httpRequest.send();
+    }
+    });
+    
+};
