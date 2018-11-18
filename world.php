57e85f4cc8669a3dd4709eb966cdbdb699014d9a
Lindsay Pink <lindsay.pink@mymona.uwi.edu>
2018-11-18 00:58:17 +0000

    updated sql query

--- world.php
+++ world.php
@@ -9,7 +9,7 @@ $country = $_GET['name'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-$stmt = $conn->query("SELECT * FROM countries");
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
